4c9ee6cfc537924e9ad09829313f7fa96901546a
Simon Kye <kkeysimon@gmail.com>
2024-02-07 18:34:04 -0500

    added button and text

--- app/src/main/java/com/example/simonkye_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/simonkye_lifecycle/MainActivity.kt
@@ -3,12 +3,28 @@ package com.example.simonkye_lifecycle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editText: EditText
+    private lateinit var textView: TextView
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("TAG", "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener{ view: View ->
+            textView.setText("Hello")
+            editText.setText("Hello")
+        }
     }
     override fun onStart() {
         super.onStart()
