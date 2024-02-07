b538c78a7bd7c9673b19c935de5f54caf23c1c24
Simon Kye <kkeysimon@gmail.com>
2024-02-07 18:11:51 -0500

    logs implemented

---
+++ app/src/main/java/com/example/simonkye_lifecycle/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.simonkye_lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("TAG", "onCreate(Bundle?) called")
+        setContentView(R.layout.activity_main)
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG", "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG", "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG", "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG", "onStop() called")
+    }
+}
